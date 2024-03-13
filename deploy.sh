fc8d4b98a03427eb19bd1b0f8ea80b8033a93de5
Rajamahendarreddy777 <153619291+Rajamahendarreddy777@users.noreply.github.com>
2024-03-13 14:50:27 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 docker login -u rajamahendarreddy777 -p dckr_pat_Eg5J3X0S19l0qx3v3AnUI3HoEpU
 
 # Tag the Docker image with a specific repository and tag name(our wish)
-docker tag devops-build_app rajamahendarreddy777/prod
+docker tag devops-build_app rajamahendarreddy777/dev
 
 # Push the tagged Docker image to Docker Hub.
-docker push rajamahendarreddy777/prod
+docker push rajamahendarreddy777/dev
 
 # Start the Docker Compose services in detached mode
 docker-compose up -d
